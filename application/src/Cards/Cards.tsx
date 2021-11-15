9674146d24855bad3dcb9c28021d9132a062d6d1
NikhilSingh <nicknirvana70@gmail.com>
2021-11-16 00:15:36 +0530

    added_cards

---
+++ application/src/Cards/Cards.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Card, Container, Row, CardGroup, Col, Button } from "react-bootstrap";
+
+const Cards = () => {
+    return (
+        <React.Fragment>
+            <div className="cards">
+                <Container>
+                    <Row>
+                        <Col lg="4">
+                            <Card >
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col lg="4" md="6">
+                            <Card >
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col lg="4">
+                            <Card >
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Cards;
--- application/src/Content/Home.tsx
+++ application/src/Content/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import Content from "./Content"
+import Content from "./Content";
+import Cards from "../Cards/Cards";
 const Home = () =>{
     return (<React.Fragment>
         <Content />
+        <Cards />
     </React.Fragment>)
 }
 export default Home;
