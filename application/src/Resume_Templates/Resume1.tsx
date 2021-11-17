1ae8096a51aed591dfc65e9f236cd018d3871685
NikhilSingh <nicknirvana70@gmail.com>
2021-11-17 23:52:18 +0530

    adding_structure_of_resume_template1

--- application/src/Resume_Templates/Resume1.tsx
+++ application/src/Resume_Templates/Resume1.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
-import {Form} from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Resume1 = () => {
     return (
         <React.Fragment>
-            <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
-                </Form.Group>
-            </Form>
+            <div>
+                <Form style={{margin:'1%', padding:"0.5%"}}>
+                    <Form.Group className="mb-2" controlId="formGeneralInformation">
+                        <Form.Label column sm={2}> Enter your Full_Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter your name"></Form.Control>
+                    </Form.Group>
+                </Form>
+            </div>
+            <div>
+                <Form >
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+                <Form >
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea1</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+                <Form >
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea2</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+                <Form >
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea3</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+                <Form >
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea4</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+                <Form >
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea5</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+            </div>
         </React.Fragment>
     );
 }
