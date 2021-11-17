6cefb34ec9e65fbd3c4a3dddb2ef599731477187
harshit <harshit.raghav_cs19@gla.ac.in>
2021-11-17 23:15:53 +0530

    create_ link_ with_ button_ in_cards

--- application/src/Cards/Cards.tsx
+++ application/src/Cards/Cards.tsx
@@ -18,7 +18,7 @@ const Cards = () => {
                                         the card's content.
                                     </Card.Text>
                                     {/* <Button variant="primary">Go somewhere</Button> */}
-                                    <Link to = "/Resume1">Resume1</Link>
+                                    <Link to = "/Resume1"><Button variant="primary">Go somewhere</Button></Link>
                                 </Card.Body>
                             </Card>
                         </Col>
