7133f9199cc481c44df558e1ba8ec088f7568d19
harshit <harshit.raghav_cs19@gla.ac.in>
2021-11-24 01:33:13 +0530

    removed header from home

--- application/src/Content/Home.tsx
+++ application/src/Content/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Content from "./Content";
 import Cards from "../Cards/Cards";
+import Header from "../Header/Header";
 const Home = () =>{
     return (<React.Fragment>
+        {/* <Header /> */}
         <Content />
         <Cards />
     </React.Fragment>)
