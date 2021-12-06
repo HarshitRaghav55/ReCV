6cc041a88848fb9ce6a97880100a5febdfd35986
sakshamgupta1802 <saksham.gupta_cs19@gla.ac.in>
2021-12-06 20:39:41 +0530

    upload

--- application/src/CV1/CV1.tsx
+++ application/src/CV1/CV1.tsx
@@ -344,10 +344,7 @@ const CV1 = () =>{
     </div>
 </div>
    
-    <footer className="text-center">
-        <p>&copy; <a href="http://creativecommons.org/licenses/by/3.0/">CC-3.0</a>, Just another CV template by <a href="https://evenfly.com">EvenFly </a> - Designed by <a target="_blank" href="http://www.facebook.com/towkirahmedbappy">Towkir Ahmed Bappy</a>.
-        </p>
-    </footer>
+    
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
