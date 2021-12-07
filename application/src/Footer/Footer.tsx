e32df04b8134e77726533ae3988c35772e2f952f
sakshamgupta1802 <saksham.gupta_cs19@gla.ac.in>
2021-12-07 16:50:20 +0530

    m

--- application/src/Footer/Footer.tsx
+++ application/src/Footer/Footer.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../Footer/'
+import '../Footer/Footer.css';
 const Footer = () => {
     return (
         <React.Fragment>
